0436ad4a260e82831176ad15cd9c25d0285037c3
Constanza Galía Maldonado <coco.galiam@gmail.com>
2021-08-05 21:58:59 -0300

    ABM categorías, falta el, create and update

--- models/categorias.js
+++ models/categorias.js
@@ -1,9 +1,43 @@
 const pool = require('../utils/db');
 
 const getCategorias = async () => {
-  const query = "SELECT * FROM ??"
-  const params = [process.env.T_CATEGORIAS]
-  return await pool.query(query, params);
+  try {
+    const query = "SELECT * FROM ?? WHERE eliminado = 0"
+    const params = [process.env.T_CATEGORIAS]
+    return await pool.query(query, params); 
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-module.exports = {getCategorias}
+const deleteCategory = async (id) => {
+  try {
+    const query = "UPDATE ?? SET eliminado = 1 WHERE id = ?";
+    const params = [process.env.T_CATEGORIAS, id];
+    return await pool.query(query, params);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getSingleCategory = async (id) => {
+  try {
+    const query = "SELECT c.nombre, c.id FROM ?? AS c WHERE id = ?";
+    const params = [process.env.T_CATEGORIAS, id];
+    return await pool.query(query, params);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updateCategory = async (id, obj) => {
+  try {
+    const query = "UPDATE ?? SET ? WHERE id = ?";
+    const params = [process.env.T_CATEGORIAS, obj, id];
+    return await pool.query(query, params);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = {getCategorias, deleteCategory, getSingleCategory, updateCategory}
--- routes/admin/categorias.js
+++ routes/admin/categorias.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const model = require('../../models/categorias');
 
 
+const getAllCategories = async (req, res) => {
+  const categorias = await model.getCategorias();
+  res.render('adminCategorias', { categorias})
+}
+
+const deleteCategorias = async (req, res) => {
+  const {id} = req.params;
+  console.log(id)
+  await model.deleteCategory(id);
+  res.redirect('/admin/categorias');
+}
+
+router.get('/', getAllCategories);
+router.get('/delete/:id', deleteCategorias);
 module.exports = router;
